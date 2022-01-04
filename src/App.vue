08042f1b2ef21761279f8972eacd08c605127756
benily <leekwokhinben@gmail.com>
2022-01-04 22:39:31 +0800

    update data when user fetch this page

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,27 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
-  <router-view/>
+  <div id="nav"></div>
+  <router-view />
 </template>
+
+<script>
+import { watch } from "vue";
+import { useStore } from "vuex";
+export default {
+  setup() {
+    const store = useStore();
+
+    store.dispatch("getUserLocation");
+
+    return {
+      ip: watch(
+        () => store.state.ip,
+        () => store.dispatch("getWeatherInfo")
+      ),
+    };
+  },
+};
+</script>
+
 
 <style>
 #app {
